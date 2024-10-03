01bedf1a5f4b6e2b9d713d455647a820e36a7622
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-10-03 01:50:59 +0100

    created the sprint contents

--- src/components/Sprint.tsx
+++ src/components/Sprint.tsx
@@ -3,6 +3,22 @@ import React from "react";
 
 export default function Sprint() {
   return (
-    <div>Sprint</div>
-  )
+    <div className="container flex gap-10 pb-40">
+      <div>
+        <h1 className="ml-20 text-5xl font-bold pb-10">What is Sprint Evaluation?</h1>
+        <p className="ml-20 text-2xl ">
+          The sprint review meeting allows the scrum team to assess its
+          performance and devise strategies for enhancements to be implemented
+          in the upcoming sprint.
+        </p>
+      </div>
+      <div>
+        <img
+          src="/images/undraw_Engineering_team_a7n2.png"
+          alt="engineering-sprint-section"
+          className=" rounded-l-full"
+        />
+      </div>
+    </div>
+  );
 }
