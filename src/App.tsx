8c72f0173fbf7a847a59a809eaea8ab4838cce5c
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-10-03 01:12:41 +0100

    created the hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <main className="min-h-screen dark:bg-gray-800">
-     <Navbar />
-      
+    <main className="min-h-screen text-gray-800 dark:bg-gray-800 bg-sky-100 dark:text-gray-200">
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Hero />
+      </div>
     </main>
   );
 }
