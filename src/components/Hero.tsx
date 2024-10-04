04b0db629eed4d2f8e34485d0b0bf6e2d7f1c5a0
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-10-04 15:37:38 +0100

    created a new hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,32 +2,47 @@ import React from "react";
 
 const Hero: React.FC = () => {
   return (
-    <div className="pt-56 pb-56">
-      <div className="mx-20">
-        <h1 className="text-center text-6xl font-bold">
-          Enhance Team Collaboration with
-        </h1>
-        <h1 className="text-center text-6xl font-bold">
-          Effective Agile Retrospectives
-        </h1>
-        <p className="mx-72 pt-6 text-center text-xl font-semibold md:text-3xl">
-          Drive continuous improvement and
-        </p>
-        <p className="mx-72 text-center text-xl font-semibold md:text-3xl">
-          elevate your workflow.
-        </p>
-        <p className="mx-72 text-center text-xl font-semibold md:text-3xl">
-          Absolutely free!
-        </p>
-        <div className="my-6 flex items-center justify-center">
-          <button className="rounded-md bg-blue-700 px-7 py-3 font-bold text-white hover:bg-blue-600">
-            Start Retro
-          </button>
-          <button className="mx-4 rounded-md bg-gray-400 px-7 py-3 font-bold text-white hover:bg-gray-500">
-            Create Team
-          </button>
+    <div>
+      <section className="mb-20 bg-gray-700 bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-center bg-no-repeat bg-blend-multiply">
+        <div className="mx-auto max-w-screen-xl px-4 py-24 text-center lg:py-56">
+          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-6xl">
+            Enhance Team Collaboration with Effective Agile Retrospectives
+          </h1>
+          <p className="mb-8 text-lg font-normal text-gray-300 sm:px-16 lg:px-48 lg:text-xl">
+            Drive continuous improvement and elevate your workflow. Absolutely
+            free!
+          </p>
+          <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+            <a
+              href="#"
+              className="inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+            >
+              Start Retro
+              <svg
+                className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+            <a
+              href="#"
+              className="inline-flex items-center justify-center rounded-lg border border-white px-5 py-3 text-center text-base font-medium text-white hover:bg-gray-100 hover:text-gray-900 focus:ring-4 focus:ring-gray-400 sm:ms-4"
+            >
+              Create Team
+            </a>
+          </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 };
