4a17ad26ccf29aedc1d00e3a30b64221d6d1d508
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-10-05 12:13:25 +0100

    made the hero section resposive

--- src/components/sprint/Sprint.tsx
+++ src/components/sprint/Sprint.tsx
@@ -12,12 +12,12 @@ interface SprintProps {
 export default function Sprint({ sprintDetails }: SprintProps) {
   return (
     <div className=" pb-40">
-      <div className="container flex gap-10">
+      <div className="container md:flex md:gap-10">
         <div className=" ">
-          <h1 className="ml-20 pb-10 text-5xl font-bold">
+          <h1 className="ml-10 md:ml-20 pb-10 text-2xl md:text-5xl font-bold">
             What is Sprint Evaluation?
           </h1>
-          <p className="ml-20 text-2xl">
+          <p className="ml-10 md:ml-20 md:text-2xl">
             The sprint review meeting allows the scrum team to assess its
             performance and devise strategies for enhancements to be implemented
             in the upcoming sprint.
@@ -27,13 +27,13 @@ export default function Sprint({ sprintDetails }: SprintProps) {
           <img
             src="/images/undraw_real_time_collaboration_c62i.png"
             alt="engineering-sprint-section"
-            className=" rounded-l-full"
+            className="mt-10 rounded-lg md:rounded-l-full"
           />
         </div>
       </div>
 
       {/* Map over sprintDetails and render each item */}
-      <div className="mt-40 grid grid-cols-2 gap-10 px-40 ">
+      <div className="mt-20 mx-10 md:mt-40 md:grid md:grid-cols-2 gap-10 md:px-40 ">
         {sprintDetails.map(({ id, Icon, title, body }) => (
           <div key={id} className=" mb-10  items-start">
             <figure>
