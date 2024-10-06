c4f28d9fd087522d860093b2c52246a2dc216889
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-10-06 11:25:48 +0100

    cleared some unncessary comments

--- src/components/publicretro/PublicRetro..ts
+++ src/components/publicretro/PublicRetro..ts
@@ -1,32 +1,3 @@
-// import { IoIosLink } from "react-icons/io";
-// import { GrStatusGood } from "react-icons/gr";
-// import { FaRegCircle } from "react-icons/fa";
-
-// export const pubRetro = [
-//   {
-//     id: 1,
-//     icon: IoIosLink,
-//     title: "Unlimited public retros",
-//   },
-//   {
-//     id: 2,
-//     icon: GrStatusGood,
-//     title: "Real-time collaboration",
-//   },
-//   {
-//     id: 3,
-//     icon: GrStatusGood,
-//     title: "Issue voting",
-//   },
-//   {
-//     id: 4,
-//     icon: FaRegCircle,
-//     title: "Export options (coming soon)",
-//   },
-// ];
-
-
-
  import { IoIosLink } from "react-icons/io";
  import { GrStatusGood } from "react-icons/gr";
  import { FaRegCircle } from "react-icons/fa";
