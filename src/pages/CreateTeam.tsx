6ef08ce47d29012d1847fc9975d9291c89c9b2a3
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-10-07 22:26:41 +0100

    added the content of the create retro page

--- src/pages/CreateTeam.tsx
+++ src/pages/CreateTeam.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export default function CreateTeam() {
   return (
-    <div className="min-h-screen pt-40">CreateTeam</div>
-  )
+    <div className="min-h-screen pt-72 md:pt-52">
+      <div className="flex items-center justify-center">
+        <div className=" p-10 rounded-lg w-96 border-2">
+          <form>
+            <label className="block text-xl font-bold  dark:text-white">Create Team</label>
+            <input type="text" className="w-full" />
+            <button className="bg-blue-700 text-white w-full hover:bg-blue-600 px-10 py-3 rounded-md mt-4 font-bold">Create</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
