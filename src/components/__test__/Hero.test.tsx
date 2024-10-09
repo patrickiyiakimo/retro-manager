f2e398e24b69501b559664841ac92ab65c1a0ea2
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-10-09 01:08:02 +0100

    wrote the unit test for the hero page

--- src/components/__test__/Hero.test.tsx
+++ src/components/__test__/Hero.test.tsx
@@ -1,7 +1,6 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Hero from "../Hero";
 import { BrowserRouter } from "react-router-dom";
-
 
 const renderHero = () => {
   return render(
@@ -9,11 +8,49 @@ const renderHero = () => {
       <Hero />
     </BrowserRouter>,
   );
-}
+};
 
 describe("Hero Component", () => {
   it("renders the background image", () => {
+    renderHero();
 
+    const sectionElement = screen.getByRole("region");
+    expect(sectionElement).toHaveStyle({
+      backgroundImage:
+        "url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')",
+    });
+  });
+  it("renders the heading text correctly", () => {
+    renderHero();
+
+    const headingElement = screen.getByRole("heading", {
+      name: /Enhance Team Collaboration with Effective Agile Retrospectives/i,
+    });
+    expect(headingElement).toBeInTheDocument();
   });
 
-})
+  it("renders paragraph text correctly", () => {
+    renderHero();
+
+    const paragraphElement = screen.getByText(
+      /drive continuous improvement and elevate your workflow. absolutely free!/i,
+    );
+    expect(paragraphElement).toBeInTheDocument();
+  });
+
+  it("renders the start retro link", () => {
+    renderHero();
+
+    const startRetroLink = screen.getByRole("link", { name: /start retro/i });
+    expect(startRetroLink).toBeInTheDocument();
+    expect(startRetroLink).toHaveAttribute("href", "/retrosection");
+  });
+
+  it("renders the create team link", () => {
+    renderHero();
+
+    const createTeamLink = screen.getByRole("link", { name: /create team/i });
+    expect(createTeamLink).toBeInTheDocument();
+    expect(createTeamLink).toHaveAttribute("href", "/createteam");
+  });
+});
