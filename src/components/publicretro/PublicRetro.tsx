a0eba107be35843e50b8382e8a137c56475cd27d
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-10-09 15:27:03 +0100

    made some chaanges in the sprint page

--- src/components/publicretro/PublicRetro.tsx
+++ src/components/publicretro/PublicRetro.tsx
@@ -15,7 +15,7 @@ export default function Sprint({ publicRetro }: RetroProps) {
       <h1 className="text-center text-4xl font-bold">Public Retro</h1>
 
       {/* Map over sprintDetails and render each item */}
-      <div className="ml-10 md:ml-10 lg:ml-32  mt-10 sm:grid sm:grid-cols-2  items-center justify-center sm:gap-5 md:gap-10 md:px-10 lg:px-40">
+      <div className="ml-10 mt-10   items-center justify-center sm:grid  sm:grid-cols-2 sm:gap-5 md:ml-10 md:gap-10 md:px-10 lg:ml-32 lg:px-40">
         {publicRetro.map(({ id, Icon, title }) => (
           <div key={id} className=" mb-10  items-start">
             <figure>
@@ -30,9 +30,9 @@ export default function Sprint({ publicRetro }: RetroProps) {
       </div>
       <div className="flex items-center justify-center">
         <Link to="/retrosection">
-         <button className="inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-          Start Free Retro 
-        </button>
+          <button className="inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+            Start Free Retro
+          </button>
         </Link>
       </div>
     </div>
--- src/components/sprint/Sprint.tsx
+++ src/components/sprint/Sprint.tsx
@@ -12,28 +12,28 @@ interface SprintProps {
 export default function Sprint({ sprintDetails }: SprintProps) {
   return (
     <div className="pb-20 md:pb-40">
-      <div className="container lg:flex md:gap-10">
+      <div className="container md:gap-10 lg:flex ">
         <div className=" ">
-          <h1 className="ml-10 md:ml-10 pb-10 text-2xl md:text-5xl font-bold lg:mt-40">
+          <h1 className="ml-10 pb-10 text-2xl font-bold md:ml-10 md:text-5xl lg:mt-40">
             What is Sprint Evaluation?
           </h1>
           <p className="ml-10 md:ml-10 md:text-2xl">
             The sprint review meeting allows the scrum team to assess its
-            performance and devise strategies for <br/> enhancements to be implemented
-            in the upcoming sprint.
+            performance and devise strategies for <br /> enhancements to be
+            implemented in the upcoming sprint.
           </p>
         </div>
         <div>
           <img
             src="/images/main-pic-68043f7e0b9a77d65db913dd43a9da06.png"
             alt="engineering-sprint-section"
-            className="mt-10 ml-10 sm:px-10  sm:w-full rounded-lg  md:rounded-l-full w-3/4 "
+            className="ml-10 mt-10 w-3/4  rounded-lg sm:w-full  sm:px-10 md:rounded-l-full "
           />
         </div>
       </div>
 
       {/* Map over sprintDetails and render each item */}
-      <div className="mt-20 mx-10 md:mt-40 md:grid md:grid-cols-2 lg:grid lg:grid-cols-2 gap-10 md:px-10 ">
+      <div className="mx-10 mt-20 gap-10 md:mt-40 md:grid md:grid-cols-2 md:px-10 lg:grid lg:grid-cols-2 ">
         {sprintDetails.map(({ id, Icon, title, body }) => (
           <div key={id} className=" mb-10  items-start">
             <figure>
