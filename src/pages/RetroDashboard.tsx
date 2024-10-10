031d5399820fb74fe32cc94d68fd6beaf9998818
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-10-10 12:03:37 +0100

    added an action to the retro dashboard

--- src/pages/RetroDashboard.tsx
+++ src/pages/RetroDashboard.tsx
@@ -10,7 +10,7 @@ export default function RetroDashboard() {
   };
 
   return (
-    <div className="min-h-screen pt-40">
+    <div className="min-h-screen px-5 pt-40">
       <div className="relative overflow-x-auto">
         <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400 rtl:text-right">
           <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
@@ -23,6 +23,9 @@ export default function RetroDashboard() {
               </th>
               <th scope="col" className="px-6 py-3">
                 Participant
+              </th>
+               <th scope="col" className="px-6 py-3">
+                Action
               </th>
             </tr>
           </thead>
