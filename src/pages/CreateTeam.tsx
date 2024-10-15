3c4ce97ef7ea6b905b685a072d4845d26e1c9d66
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-10-15 12:57:37 +0100

    changed the create team text

--- src/pages/CreateTeam.tsx
+++ src/pages/CreateTeam.tsx
@@ -6,7 +6,7 @@ export default function CreateTeam() {
       <div className="flex items-center justify-center">
         <div className=" p-10 rounded-lg w-96 border-2 dark:border-gray-700">
           <form>
-            <label className="block text-xl font-bold pb-2 dark:text-white">Create Team</label>
+            <label className="block text-xl font-bold pb-2 dark:text-white">Generate Team ID</label>
             <input type="text" className="w-full rounded-lg dark:bg-gray-600 border-gray-600 " />
             <button className="bg-blue-700 text-white w-full hover:bg-blue-600 px-10 py-3 rounded-md mt-4 font-bold">Create</button>
           </form>
