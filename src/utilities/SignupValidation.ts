b892d379e18629f7d4094cb906fe5d28032f6912
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-10-20 00:10:37 +0100

    created the signup form validation

--- src/utilities/SignupValidation.ts
+++ src/utilities/SignupValidation.ts
@@ -1,1 +1,14 @@
-import React from "react";
+import * as Yup from 'yup';
+
+export const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .required('Name is required')
+    .min(2, 'Name must be at least 2 characters long'),
+  email: Yup.string()
+    .required('Email is required')
+    .email('Email is not valid'),
+  password: Yup.string()
+    .required('Password is required')
+    .min(6, 'Password must be at least 6 characters long'),
+  remember: Yup.boolean(),
+});
