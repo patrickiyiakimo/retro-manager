0f79c2a1be237c1bff956fbb354d6a1174fc14ac
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-10-21 18:02:00 +0100

    refactored my signup validation code

--- src/utilities/SignupValidation.ts
+++ src/utilities/SignupValidation.ts
@@ -1,14 +1,22 @@
-import * as Yup from 'yup';
+import * as Yup from "yup";
+
+export const nameValidator = Yup.string()
+  .required("Name is required")
+  .min(3, "Name must be at least 3 characters long");
+
+export const emailValidator = Yup.string()
+  .required("Email is required")
+  .email("Email is not valid");
+
+export const passwordValidator = Yup.string()
+  .required("Password is required")
+  .min(6, "Password must be at least 6 characters long");
+
+export const rememberValidator = Yup.boolean();
 
 export const validationSchema = Yup.object().shape({
-  name: Yup.string()
-    .required('Name is required')
-    .min(2, 'Name must be at least 2 characters long'),
-  email: Yup.string()
-    .required('Email is required')
-    .email('Email is not valid'),
-  password: Yup.string()
-    .required('Password is required')
-    .min(6, 'Password must be at least 6 characters long'),
-  remember: Yup.boolean(),
+  name: nameValidator,
+  email: emailValidator,
+  password: passwordValidator,
+  remember: rememberValidator,
 });
