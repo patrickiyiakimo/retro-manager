aea71c0de5c6252b58e0ee7a3c724698207a83f6
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-10-28 12:03:24 +0100

    fetched the api for user registration

--- src/api/SignupUser.ts
+++ src/api/SignupUser.ts
@@ -1,1 +1,32 @@
-import React from "react";
+interface UserData{
+    username: string;
+    email: string;
+    password: string;
+}
+
+
+export const signup = async (userData: UserData): Promise<any> => {
+  try {
+    const response = await fetch(
+      "https://retro-manager-server.vercel.app/register",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      },
+    );
+
+    if (!response.ok) {
+      const errorData = await response.json(); 
+      throw new Error(errorData.message || "Signup Failed"); 
+    }
+
+    const data = await response.json(); 
+    return data;
+  } catch (error) {
+    console.error("Error message:", error);
+    throw error; 
+  }
+};
