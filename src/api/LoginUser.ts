50fa35fa2767221a8b49bb6230611e54b7bc96cd
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-10-30 23:12:07 +0100

    created the user login

--- src/api/LoginUser.ts
+++ src/api/LoginUser.ts
@@ -1,1 +1,22 @@
-import React from "react"
+export const login = async( userData: {
+    email: string;
+    password: string;
+}) => {
+    try {
+        const response = await fetch("http://localhost:2500/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userData)
+        });
+
+        if (!response.ok) {
+            throw new Error("Failed to login")
+        }
+        return await response.json();
+    } catch (error) {
+        console.error("Error during login:", error);
+        throw error;
+    }
+}
