44403cafb44fa275f3ced40c0cfdef14a45f4569
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-11-04 13:38:46 +0100

    mapped the faq ts file

--- src/components/FrequentlyAskedQuestions.tsx
+++ src/components/FrequentlyAskedQuestions.tsx
@@ -1,21 +1,24 @@
-import { faq } from "./Faq"; 
+import React from "react";
+import { faq } from "./Faq";
 import { Accordion } from "flowbite-react";
 
 export default function FrequentlyAskedQuestions() {
   return (
-    <div className="mt-40">
-      <h1 className="mb-4 text-2xl font-bold">Frequently Asked Questions</h1>
-      <Accordion collapseAll className="space-y-4">
-        <Accordion.Panel>
-          {faq.map((item, index) => (
-            <div key={index} className="rounded-md border p-4 shadow-md">
-              <Accordion.Title>{item.title}</Accordion.Title>
-              <Accordion.Content>
-                <p className="mb-2 text-gray-800 dark:text-white">{item.body}</p>
-              </Accordion.Content>
-            </div>
-          ))}
-        </Accordion.Panel>
+    <div className="py-40 min-h-screen md:mx-40">
+      <h1 className="mb-4 text-center text-2xl font-bold">
+        Frequently Asked Questions
+      </h1>
+      <Accordion alwaysOpen={false}>
+        {faq.map((item, index) => (
+          <Accordion.Panel key={index}>
+            <Accordion.Title className="mb-2 text-gray-800 dark:text-white">
+              {item.title}
+            </Accordion.Title>
+            <Accordion.Content>
+              <p className="mb-2 text-gray-800 dark:text-white">{item.body}</p>
+            </Accordion.Content>
+          </Accordion.Panel>
+        ))}
       </Accordion>
     </div>
   );
