fc26b710901aba96daa44518ca1191da1091df3b
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-11-04 13:43:38 +0100

    made changes in the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -66,14 +66,15 @@ const Navbar: React.FC = () => {
           id="navbar-sticky"
         >
           <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900 rtl:space-x-reverse">
-            <li>
-              <a
-                href="#"
-                className="mt-3 block rounded bg-blue-700 px-3 py-2 text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500" aria-current="page"
+            <Link to="/">
+              <li
+                className="mt-3 block rounded bg-blue-700 px-3 py-2 text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
+                aria-current="page"
               >
                 Home
-              </a>
-            </li>
+              </li>
+            </Link>
+
             <li>
               <a
                 href="#"
@@ -83,9 +84,7 @@ const Navbar: React.FC = () => {
               </a>
             </li>
             <Link to="/services">
-              <li
-                className="mt-3 block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
-              >
+              <li className="mt-3 block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500">
                 Services
               </li>
             </Link>
