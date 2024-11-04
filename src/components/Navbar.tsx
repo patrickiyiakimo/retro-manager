f448c301ea0aa2fcf22e9f8c561ec77965804528
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-11-04 13:53:36 +0100

    made changes in the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -74,15 +74,13 @@ const Navbar: React.FC = () => {
                 Home
               </li>
             </Link>
-
-            <li>
-              <a
-                href="#"
-                className="mt-3 block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
-              >
+            
+            <Link to="/about">
+              <li className="mt-3 block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500">
                 About
-              </a>
-            </li>
+              </li>
+            </Link>
+
             <Link to="/services">
               <li className="mt-3 block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500">
                 Services
