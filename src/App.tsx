cca000e67f8228803be618f628af570a879010e3
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-11-05 11:32:56 +0100

    created the about section

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import RetroDashboard from "./pages/RetroDashboard";
 import InviteTeamMembers from "./pages/invites/InviteTeamMembers";
 import Services from "./components/Services";
 import FrequentlyAskedQuestions from "./components/FrequentlyAskedQuestions";
+import About from "./components/About";
 
 
 
@@ -51,7 +52,7 @@ function App() {
           <Route path="/inviteteam" element={<InviteTeamMembers />} />
           <Route path="/services" element={<Services />} />
           <Route path="/faq" element={<FrequentlyAskedQuestions />} />
-          {/* <Route path="/services" element={<Faq />} /> */}
+          <Route path="/about" element={<About />} />
         </Routes>
 
         <Footer />
