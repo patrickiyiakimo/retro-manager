cb6d2a588aea98278dad066c71c7aaa287183e58
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-11-06 18:28:52 +0100

    made some text change

--- src/layout/PublicRetro.tsx
+++ src/layout/PublicRetro.tsx
@@ -16,14 +16,10 @@ export default function Sprint({ publicRetro }: RetroProps) {
   return (
     <div className="py-20 text-gray-800 dark:text-white">
       <h1 className="text-center text-4xl font-bold">Our Mission</h1>
-
-      {/* Map over publicRetro and render each item */}
       <div className="my-10 grid gap-5 sm:grid-cols-2 md:ml-10 md:gap-10 md:px-10 lg:ml-32 lg:px-32">
         {publicRetro.map(({ id, Icon, title, body, image }) => (
           <Card key={id} className="max-w-sm">
             <figure className="overflow-hidden">
-              {" "}
-              {/* Add overflow-hidden to the figure */}
               <img
                 src={image}
                 alt={`${title} logo`}
