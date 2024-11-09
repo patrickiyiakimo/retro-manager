acfd26657e0aad5eec46b10eb8ebfdcff6ee46fe
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-11-09 13:06:01 +0100

    removed some dirty comments

--- src/pages/invites/InviteManager.tsx
+++ src/pages/invites/InviteManager.tsx
@@ -1,45 +1,4 @@
-// import React, { useEffect, useState } from "react";
-// import InvitesTable from "./InvitesTable";
-// import InviteTeamModal from "./InviteTeamModal";
-// import { fetchInvites } from "../../api/FetchInvites";
-
-// interface Invite {
-//   email: string;
-//   position?: string;
-// }
-
-// export default function InviteManager() {
-//   const [invites, setInvites] = useState<Invite[]>([]);
-
-//   const fetchData = async () => {
-//     try {
-//       const response = await fetchInvites();
-//       setInvites(response);
-//     } catch (error) {
-//       console.error("Error fetching invites:", error);
-//     }
-//   };
-
-//   useEffect(() => {
-//     fetchData();
-//   }, []);
-
-//   const addInvite = (newInvite: Invite) => {
-//     setInvites((prevInvites) => [...prevInvites, newInvite]);
-//   };
-
-//   return (
-//     <div>
-//       <InviteTeamModal addInvite={addInvite} />
-//       <InvitesTable invites={invites} />
-//     </div>
-//   );
-// }
-
-
-
-
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import InvitesTable from "./InvitesTable";
 import InviteTeamModal from "./InviteTeamModal";
 import { fetchInvites } from "../../api/FetchInvites";
