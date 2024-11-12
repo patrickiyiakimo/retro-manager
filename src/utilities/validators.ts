31df279e950e513f9a3bb9117f0551c1e1220898
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-11-12 10:23:46 +0100

    added more validation to yup validation schema

--- src/utilities/validators.ts
+++ src/utilities/validators.ts
@@ -8,9 +8,28 @@ export const emailValidator = Yup.string()
   .required("Email is required")
   .email("Email is not valid");
 
-export const passwordValidator = Yup.string()
-  .required("Password is required")
-  .min(6, "Password must be at least 6 characters long");
+  export const passwordValidator = Yup.mixed()
+    .required("Password is required")
+    .test(
+      "is-string-or-number",
+      "Password must be a string or a number",
+      (value) => {
+        return typeof value === "string" || typeof value === "number";
+      },
+    )
+    .test(
+      "min-length",
+      "Password must be at least 6 characters long",
+      (value) => {
+        if (typeof value === "string") {
+          return value.length >= 6;
+        }
+        return true; // If it's a number, we don't check length
+      },
+    );
+// export const passwordValidator = Yup.string()
+//   .required("Password is required")
+//   .min(6, "Password must be at least 6 characters long");
 
 export const rememberValidator = Yup.boolean();
 
